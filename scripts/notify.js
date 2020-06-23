407a4f15956efd93ed1d92893da0c6957eef580c
Razvan <tangent.jotey@gmail.com>
2020-06-23 19:12:20 +0200

    Update

--- scripts/notify.js
+++ scripts/notify.js
@@ -5,20 +5,39 @@ export default async function notify(title,body,vibrate=[200, 100, 200],icon='st
 		console.warn("Notification won't be sent because notification permission has not been granted.");
 		return;
 	}
-	(worker.subscribe($worker=>{
-		if($worker === null && $worker.active){
-			console.warn("You need to install the main worker before sending a notification. Please open the settigs seidemenu and click 'INSTALLA LOCALMENTE'.");
-			return;
-		}
-		$worker.active.postMessage(JSON.stringify({
-			action: "send-notification",
-			body: {
-				title,
-				body,
-				vibrate,
-				icon,
-				tag
+	if(!window.cordova){
+		(worker.subscribe($worker=>{
+			if($worker === null){
+				console.warn("You need to install the main worker before sending a notification. Please open the settigs seidemenu and click 'INSTALLA LOCALMENTE'.");
+				return;
 			}
-		}));
-	}))();
+			$worker.active.postMessage(JSON.stringify({
+				action: "send-notification",
+				body: {
+					title,
+					body,
+					vibrate,
+					icon,
+					tag
+				}
+			}));
+		}))();
+	}else{
+		// var date = new Date();
+		
+		// cordova.plugins.notification.local.schedule({
+		// 	id: 1,
+		// 	title: title,
+		// 	message: body,
+		// 	firstAt: date, // firstAt and at properties must be an IETF-compliant RFC 2822 timestamp
+		// 	//every: "week", // this also could be minutes i.e. 25 (int)
+		// 	//sound: "file://sounds/reminder.mp3",
+		// 	icon: icon,
+		// 	data: { meetingId:"123#fg8" }
+		// });
+		
+		// cordova.plugins.notification.local.on("click", function (notification) {
+		// 	console.log("notification data:",notification.data.meetingId);
+		// });
+	}
 }
--- stores/worker.js
+++ stores/worker.js
@@ -1,14 +1,35 @@
 import {writable} from 'svelte/store';
 
 const worker = writable(null);
-if ('serviceWorker' in navigator) {
+if (!window.cordova && 'serviceWorker' in navigator) {
 	navigator.serviceWorker.getRegistrations().then(registrations => {
 		if(registrations.length > 0){
-		  worker.set(registrations[0]);
+			let tmp = null;
+			for(let i = 0; i < registrations.length; i++){
+				if(registrations[i].active && window.location.origin+"/worker.js" === registrations[i].active.scriptURL){
+					tmp = registrations[i];
+					break;
+				}
+			}
+			if(tmp === null){
+				console.log("Service worker 'worker.js' not found!");
+			}else{
+				console.log("Service worker 'worker.js' found!");
+				worker.set(tmp);
+			}
+		}else{
+			(async ()=>{
+				console.log("Attempting to register service worker 'worker.js'...");
+				let res = await navigator.serviceWorker.register("worker.js");
+				console.log("Service worker 'worker.js' registration attempt result:",res);
+			})();
 		}
 	});
 }else{
-	console.warn("Service worker not found!");
+	if(window.cordova)
+		console.warn("Service will not be registered since you're running a cordova applicaiton!");
+	else
+		console.warn("Service worker 'worker.js' not found!");
 }
 
 export default worker;
