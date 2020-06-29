ebb83385b42475957642e0e0d2e8b7219fdcdfbe
Razvan <tangent.jotey@gmail.com>
2020-06-29 14:03:01 +0200

    update

---
+++ components/styles/OtherStyles.svelte
@@ -0,0 +1,17 @@
+<style>
+	:global(.full-width){
+    width: 100%;
+  }
+  :global(.full-height){
+    height: 100%;
+  }
+  :global(.border-radius-0){
+    border-radius: 0;
+  }
+	:global(.float-left){
+    float: left;
+  }
+  :global(.float-right){
+    float: right;
+  }
+</style>
