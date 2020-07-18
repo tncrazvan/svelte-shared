026faff9c4965208b910cd2b727a5902ad8876cf
Razvan <tangent.jotey@gmail.com>
2020-07-18 04:03:50 +0200

    Update

--- scripts/install.js
+++ scripts/install.js
@@ -1,21 +1,6 @@
-/**
- * Makes a request to install this website as a Progressive Web Application.
- * @param {*} worker worker relative filename
- * @returns a Promise which in turn returns the reuqest to the browser.
- * Use this request to finalize the installation.
- * Eample: 
- * let request = await install();
- *		request.prompt();
- *		request
- *			.userChoice
- *			.then((result) => {
- *				console.log(result);
- *			});
- */
-export default async function install(worker='worker.js',callback){
-	return new Promise(resolve=>{
+export default function install(callback){
+	return new Promise(async resolve=>{
 		if ('serviceWorker' in navigator) {
-			navigator.serviceWorker.register(worker);
 			window.addEventListener('beforeinstallprompt', (request) => {
 				// Prevent Chrome 67 and earlier from automatically showing the prompt
 				request.preventDefault();
--- stores/worker.js
+++ stores/worker.js
@@ -1,30 +1,31 @@
 import {writable} from 'svelte/store';
 
 const worker = writable(null);
-if (!window.cordova && 'serviceWorker' in navigator) {
-	navigator.serviceWorker.getRegistrations().then(registrations => {
-		if(registrations.length > 0){
-			let tmp = null;
-			for(let i = 0; i < registrations.length; i++){
-				if(registrations[i].active && window.location.origin+"/worker.js" === registrations[i].active.scriptURL){
-					tmp = registrations[i];
-					break;
-				}
+async function find(registrations){
+	if(registrations.length > 0){
+		let reg = null;
+		for(let i = 0; i < registrations.length; i++){
+			if(registrations[i].active && window.location.origin+"/worker.js" === registrations[i].active.scriptURL){
+				reg = registrations[i];
+				break;
 			}
-			if(tmp === null){
-				console.log("Service worker 'worker.js' not found!");
-			}else{
-				console.log("Service worker 'worker.js' found!");
-				worker.set(tmp);
-			}
-		}else{
-			(async ()=>{
-				console.log("Attempting to register service worker 'worker.js'...");
-				let res = await navigator.serviceWorker.register("worker.js");
-				console.log("Service worker 'worker.js' registration attempt result:",res);
-			})();
 		}
-	});
+		if(reg === null){
+			console.log("Service worker 'worker.js' not found!");
+		}else{
+			console.log("Service worker 'worker.js' found!");
+			worker.set(reg);
+		}
+	}else{
+		console.log("Attempting to register service worker 'worker.js'...");
+		let reg = await navigator.serviceWorker.register("worker.js");
+		worker.set(reg);
+		console.log("Service worker 'worker.js' registered!");
+	}
+}
+
+if (!window.cordova && 'serviceWorker' in navigator) {
+	navigator.serviceWorker.getRegistrations().then(find);
 }else{
 	if(window.cordova)
 		console.warn("Service will not be registered since you're running a cordova applicaiton!");
