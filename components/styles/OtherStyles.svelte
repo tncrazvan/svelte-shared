0f963f2dd7181720e2a204605084c2e2cda446b6
Razvan <tangent.jotey@gmail.com>
2020-07-18 04:14:36 +0200

    Update

--- components/styles/OtherStyles.svelte
+++ components/styles/OtherStyles.svelte
@@ -19,9 +19,14 @@
 		height: 1px;
 		background: rgba(0,0,0,0.125);
 	}
-  :global(*:not(input,textarea,p,span)){
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
+  :global(*:not(input,textarea)){
+    -webkit-touch-callout: none; /* iOS Safari */
+      -webkit-user-select: none; /* Safari */
+      -khtml-user-select: none; /* Konqueror HTML */
+        -moz-user-select: none; /* Old versions of Firefox */
+          -ms-user-select: none; /* Internet Explorer/Edge */
+              user-select: none; /* Non-prefixed version, currently
+                                    supported by Chrome, Edge, Opera and Firefox */
   }
   :global(html){
     overscroll-behavior: none;
