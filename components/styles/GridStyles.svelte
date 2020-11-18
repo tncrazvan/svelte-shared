4ba97f6ccb144ab95d738c45f7428ed75fae1ff3
Razvan <tangent.jotey@gmail.com>
2020-11-18 13:42:31 +0100

    Update GridStyles.svelte

--- components/styles/GridStyles.svelte
+++ components/styles/GridStyles.svelte
@@ -1,4 +1,38 @@
 <style>
+	:global(.grid){
+		display: grid;
+	}
+	:global(.grid-width-1){
+		grid-template-columns: 1fr;
+	}
+	:global(.grid-width-2){
+		grid-template-columns: repeat(2,1fr);
+	}
+	:global(.grid-width-3){
+		grid-template-columns: repeat(3,1fr);
+	}
+	:global(.grid-width-4){
+		grid-template-columns: repeat(4,1fr);
+	}
+	:global(.grid-width-5){
+		grid-template-columns: repeat(5,1fr);
+	}
+	:global(.grid-width-6){
+		grid-template-columns: repeat(6,1fr);
+	}
+	:global(.grid-width-7){
+		grid-template-columns: repeat(7,1fr);
+	}
+	:global(.grid-width-8){
+		grid-template-columns: repeat(8,1fr);
+	}
+	:global(.grid-width-9){
+		grid-template-columns: repeat(9,1fr);
+	}
+	:global(.grid-width-10){
+		grid-template-columns: repeat(10,1fr);
+	}
+	
 	:global(.grid-x-center){
 		display: grid;
 		justify-self: center;
