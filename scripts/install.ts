f9604f3df4e415ba513b16053842eb059a3df48a
Razvan <tangent.jotey@gmail.com>
2020-12-23 12:56:56 +0100

    Update install.ts

--- scripts/install.ts
+++ scripts/install.ts
@@ -1,33 +1,16 @@
 import worker from '../stores/worker.js';
+window.addEventListener('beforeinstallprompt', (request:any) => {
+	// Prevent Chrome 67 and earlier from automatically showing the prompt
+	request.preventDefault();
+	request.prompt();
+});
 
-export default function install(callback:Function):Promise<boolean>{
+export default function install():Promise<boolean>{
 	return new Promise(async resolve=>{
 		if ('serviceWorker' in navigator) {
 			let reg:ServiceWorkerRegistration = await navigator.serviceWorker.register("worker.js");
 			worker.set(reg);
-			window.addEventListener('beforeinstallprompt', (request:any) => {
-				// Prevent Chrome 67 and earlier from automatically showing the prompt
-				request.preventDefault();
-				request.prompt();
-				request
-					.userChoice
-					.then(async (result) => {
-						const accepted = result === 'accepted';
-						debugger;
-						if (!accepted && 'serviceWorker' in navigator) {
-							const registrations = await navigator.serviceWorker.getRegistrations();
-							if(registrations.length > 0){
-								for(let registration of registrations) {
-									registration.unregister();
-								}
-							}
-							
-							if(callback) callback(accepted);
-							resolve(request)
-						}
-						
-					});
-			});
+			
 		}else{
 			console.warn("The ServiceWorker API does not seem to be available. Make sure youre website is secure.");
 			resolve(false);
